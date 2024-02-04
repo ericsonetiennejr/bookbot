4e3447ba3a501abb686189224a55a2e534480878
Ericson Etienne Jr. <eletiennejr@gmail.com>
2024-02-03 19:53:14 -0700

    Add function generate_findings() - generates findings related to the test file. Iteration 01 Complete

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book = "books/frankenstein.txt"
+    with open(book) as f:
         file_contents = f.read()
+
     def count_words(book_string):
         words = book_string.split()
         return len(words)
+    
     def count_letters(book_string):
-        letter_dict = {}
+        char_dict = {}
         for char in book_string.lower():
-            if (char in letter_dict) == False:
-                letter_dict[char] = 1
+            if (char in char_dict) == False:
+                char_dict[char] = 1
             else:
-                letter_dict[char] += 1
-        return letter_dict
+                char_dict[char] += 1
+        return char_dict
+    
+    def sort_assist(dict):
+        for key in dict:
+            return dict[key]
 
-    count_words(file_contents)
-    count_letters(file_contents)
+    def generate_findings(book_string):
+        num_words = count_words(book_string)
+        char_dict = count_letters(book_string)
+        letter_dict = {}
+        for key in char_dict:
+            if key.isalpha():
+                letter_dict[key] = char_dict[key]
+        #This is one of the sickest things I have learned. LIST COMPREHENSION.
+        letter_list = [{k:v} for k, v in letter_dict.items()]
+        """
+        The Above code equates to:
+        letter_list = []
+        for key in letter_dict:
+            letter_list.append({key,letter_dict[key]})
+        """
+        letter_list.sort(reverse=True, key=sort_assist)
+        print(f"Generated Report for {book}:")
+        print(f"{num_words} words were found in the document \n")
+        for dict in letter_list:
+            for key in dict:
+                print(f"The letter {key} was found {dict[key]} times.")
+        print("Report Finished.")
+        
+        
+    generate_findings(file_contents)
 main()
