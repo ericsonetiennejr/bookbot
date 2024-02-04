8f0bcfea43173fc80aa162c842246d908c534f75
Ericson Etienne Jr. <eletiennejr@gmail.com>
2024-02-03 19:01:40 -0700

    Add the function count_letters(), returns a dict showing the occurrence rate of all letters in a file.

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ def main():
     def count_words(book_string):
         words = book_string.split()
         return len(words)
+    def count_letters(book_string):
+        letter_dict = {}
+        for char in book_string.lower():
+            if (char in letter_dict) == False:
+                letter_dict[char] = 1
+            else:
+                letter_dict[char] += 1
+        return letter_dict
+
     count_words(file_contents)
+    count_letters(file_contents)
 main()
